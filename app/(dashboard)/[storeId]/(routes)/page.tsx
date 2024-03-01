8862323ed0dd912df9de133d830677e88ccd89f4
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2024-03-01 23:30:53 +0700

    interim navbar creation

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,3 +1,17 @@
-export default function DashboardPage() {
-  return <div>This is a Dashboard!</div>;
+import prismadb from "@/lib/prismadb";
+
+type Props = {
+  params: { storeId: string };
+};
+
+export default async function DashboardPage({ params }: Props) {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+    },
+  });
+
+  if (!store) return null;
+
+  return <div>Active Store: {store.name}</div>;
 }
---
+++ components/navbar.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const Navbar = ({ children }: Props) => {
+  return <div>navbar</div>;
+};
